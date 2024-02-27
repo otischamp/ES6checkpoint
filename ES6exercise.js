a842f854c18f6857a63d03cefb26cb2526106c1d
Othmane Merzak <otischamp@gmail.com>
2024-02-27 09:13:57 +0100

    first commit

---
+++ ES6exercise.js
@@ -0,0 +1,18 @@
+const products = [
+    { id: 1, name: 'Laptop', price: 999.99 },
+    { id: 2, name: 'Phone', price: 699.99 },
+    { id: 3, name: 'Headphones', price: 99.99 }
+  ];
+
+  
+products.map((param) => {console.log(param)})
+
+
+
+// (param) => {return param};
+
+// console.log(products.find((param) => {return param}));
+
+// console.log(products.map((array) => {return array.name}));
+
+// console.log('this is products:',products.map((param) => {return param}));
